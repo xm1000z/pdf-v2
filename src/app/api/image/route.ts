db9cb2ac5d9d00ab94e692f4701a84b5a8d7abb1
Sam Selikoff <sam.selikoff@gmail.com>
2024-11-19 11:56:39 -0500

    wip

--- src/app/api/image/route.ts
+++ src/app/api/image/route.ts
@@ -8,16 +8,14 @@ export async function POST(req: Request) {
   const text = "text" in json ? json.text : "";
 
   const prompt = dedent`
-  I'm going to give you a short summary of what is in a PDF. I need you to create an image that captures the essence of the content.
-  
-  The image should be one that looks good as a hero image on a blog post or website. It should not include any text.
+    I'm going to give you a short summary of what is in a PDF. I need you to create an image that captures the essence of the content.
+    
+    The image should be one that looks good as a hero image on a blog post or website. It should not include any text.
 
-  Here is the summary:
+    Here is the summary:
 
-  ${text}
-`;
-
-  // await new Promise((resolve) => setTimeout(resolve, 1000));
+    ${text}
+  `;
 
   const response = await client.images.create({
     model: "black-forest-labs/FLUX.1-schnell",
